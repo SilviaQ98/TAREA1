7b0815603cfa1b9873cf189ab423923573ebf00a
SilviaQ98 <122087766+SilviaQ98@users.noreply.github.com>
2023-01-31 05:47:30 -0500

    Update compilar.bat

--- compilar.bat
+++ compilar.bat
@@ -1,13 +1,1 @@
-git clone
- && cd ./TAREA1-Distribuida 
- && cd ./app-authors 
- && gradlew quarkusBuild 
- && docker build -t jaimesalvador/app-authors:2.0.0 . 
- && cd .. 
- && cd ./app-books 
- && gradlew installDist 
- && gradlew copyLibs 
- && docker build -t jaimesalvador/app-books:2.0.0 . 
- && docker push jaimesalvador/app-books:2.0.0 
- && docker push jaimesalvador/app-authors:2.0.0 
- && cd .. && docker-compose up
+git clone https://github.com/SilviaQ98/TAREA1.git && cd ./TAREA1 && cd ./app-authors && gradlew quarkusBuild && docker build -t jaimesalvador/app-authors:2.0.0 . && cd .. && cd ./app-books && gradlew installDist && gradlew copyLibs && docker build -t jaimesalvador/app-books:2.0.0 . && docker push jaimesalvador/app-books:2.0.0 && docker push jaimesalvador/app-authors:2.0.0 && cd .. && docker-compose up
